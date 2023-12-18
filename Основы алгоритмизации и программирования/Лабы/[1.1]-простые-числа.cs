bf71a7de17f6d9109a457a45415571f5d914fc1b
mpushkarev54 <96192811+mpushkarev54@users.noreply.github.com>
2023-12-19 02:41:54 +0700

    Лаба 1.1 строго по условию
    Добавлена базовая работа по простым числам

---
+++ Основы алгоритмизации и программирования/Лабы/[1.1]-простые-числа.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SearchForPrimeNumbers {
+    class CrazyProgram {
+        static void Main() {
+
+            List<int> primesList = new List<int>();
+            
+            int target = int.Parse(Console.ReadLine());
+
+            for (int num = 2; num <= target; num++) {
+                int counter = 0;
+                for (int d = 2; d < num; d++) {
+                    if (num % d == 0) {
+                        counter++;
+                    }
+                }
+                if (counter == 0) primesList.Add(num);
+            }
+
+            foreach (int num in primesList) {
+                Console.Write($"> {num} ");
+            }
+        }
+    }
+}
