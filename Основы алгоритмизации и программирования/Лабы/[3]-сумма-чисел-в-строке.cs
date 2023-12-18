5e9407e0dff7f16ff0c2692ca739084ecd96a914
mpushkarev54 <96192811+mpushkarev54@users.noreply.github.com>
2023-12-18 22:27:35 +0700

    3 лаба
    Поиск суммы чисел в строке

---
+++ Основы алгоритмизации и программирования/Лабы/[3]-сумма-чисел-в-строке.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace sumOfNumbersInALine {
+    class CrazyProgram {
+        static void Main() {
+
+            List<string> numbersList = new List<string>();
+            bool wasDigit = false;
+
+            string inputString = Console.ReadLine();
+
+            for (int i = 0; i < inputString.Length; i++) {
+                if (inputString[i] >= '0' && inputString[i] <= '9') {
+                    string inputDigit = Convert.ToString(inputString[i]);
+                    if (wasDigit) {
+                        string lastNumber = numbersList[numbersList.Count - 1];
+                        numbersList[numbersList.Count - 1] = lastNumber + inputDigit;
+                    } else {
+                        numbersList.Add(inputDigit);
+                    }
+                    wasDigit = true;
+                } else {
+                    wasDigit = false;
+                }
+            }
+
+            int sumOfNumbers = 0;
+
+            foreach (string element in numbersList) {
+                int number = int.Parse(element);
+                sumOfNumbers += number;
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine(sumOfNumbers);
+        }
+    }
+}
