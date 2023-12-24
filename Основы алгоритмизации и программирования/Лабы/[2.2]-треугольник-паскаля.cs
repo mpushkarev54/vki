d0e91c7e06d5fee744759417f4560b70e6f92f92
mpushkarev54 <96192811+mpushkarev54@users.noreply.github.com>
2023-12-25 02:49:10 +0700

    добавлен треугольник
    Теоретически, надо было как-то через рекурсию делать, но мне лень

---
+++ Основы алгоритмизации и программирования/Лабы/[2.2]-треугольник-паскаля.cs
@@ -0,0 +1,29 @@
+﻿namespace PascalsTriangle {
+    class CrazyProgram {
+        static void Main() {
+            
+            int neededLine = int.Parse(Console.ReadLine());
+
+            int[][] triangle = new int[neededLine][];
+            
+            for (int line = 0; line < neededLine; line++) {
+                triangle[line] = new int[line + 1];
+                triangle[line][0] = 1;
+                triangle[line][line] = 1;
+            }
+
+            for (int line = 2; line < neededLine; line++) {
+                for (int cursor = 1; cursor < line; cursor++) {
+                    triangle[line][cursor] = triangle[line - 1][cursor - 1] + triangle[line - 1][cursor];
+                }
+            }
+
+            for (int line = 0; line < neededLine; line++) {
+                for (int cursor = 0; cursor <= line; cursor++) {
+                    Console.Write($" {triangle[line][cursor]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
