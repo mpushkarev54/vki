c42bd09ac7d12122e79fb9edba4c1fcf87a451ed
mpushkarev54 <96192811+mpushkarev54@users.noreply.github.com>
2023-12-24 20:33:28 +0700

    Добавлено решето
    Позже надо сделать решение лучше

---
+++ Основы алгоритмизации и программирования/Лабы/[1.2]-решето-эратосфена.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SieveOfEratosthenes {
+    class CrazyProgram {
+        static void Main() {
+            
+            int maxNum = int.Parse(Console.ReadLine());
+
+            int[] numsList = new int[maxNum - 1];
+
+            for (int num = 2; num <= maxNum; num++) {
+                numsList[num - 2] = num;
+            }
+
+            for (int numId = 0; numId < numsList.Length; numId++) {
+
+                if (numsList[numId] != -1) {
+
+                    for (int num = numsList[numId] * 2; num - 2 < numsList.Length; num += numsList[numId]) {
+                        numsList[num - 2] = -1;
+                    }
+                }
+            }
+
+            for (int numId = 0; numId < numsList.Length; numId++) {
+
+                if (numsList[numId] != -1) {
+                    Console.Write($"{numsList[numId]} ");
+                }
+            }
+        }
+    }
+}
