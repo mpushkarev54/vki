f183d4b202b962086ce472b81667b723833f0df5
mpushkarev54 <96192811+mpushkarev54@users.noreply.github.com>
2023-12-25 04:09:50 +0700

    Прямой поиск (4.1)
    Добавлена лаба

---
+++ Основы алгоритмизации и программирования/Лабы/[4.1]-прямой-поиск.cs
@@ -0,0 +1,20 @@
+﻿namespace DirectSearch {
+    class CrazyProgram {
+        static void Main() {
+            
+            string str = Console.ReadLine();
+            string q = Console.ReadLine();
+
+            Console.WriteLine(DirectSearch(str, q));
+        }
+
+        static int DirectSearch(string str, string q) {
+            for (int start = 0; start <= str.Length - q.Length; start++) {
+                if (str.Substring(start, q.Length) == q) {
+                    return start;
+                }
+            }
+            return -1;
+        }
+    }
+}
