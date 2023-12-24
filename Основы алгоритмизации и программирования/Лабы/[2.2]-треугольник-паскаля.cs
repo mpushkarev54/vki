2d2b279532eb9d991bc804b8da22c2c49bcd9e87
mpushkarev54 <96192811+mpushkarev54@users.noreply.github.com>
2023-12-25 03:15:47 +0700

    исправлен баг, добавлена выдача строки
    По-прежнему рекурсия откладывается

--- Основы алгоритмизации и программирования/Лабы/[2.2]-треугольник-паскаля.cs
+++ Основы алгоритмизации и программирования/Лабы/[2.2]-треугольник-паскаля.cs
@@ -3,27 +3,32 @@
         static void Main() {
             
             int neededLine = int.Parse(Console.ReadLine());
+            int neededLines = neededLine + 1;
 
-            int[][] triangle = new int[neededLine][];
+            int[][] triangle = new int[neededLines][];
             
-            for (int line = 0; line < neededLine; line++) {
+            for (int line = 0; line < neededLines; line++) {
                 triangle[line] = new int[line + 1];
                 triangle[line][0] = 1;
                 triangle[line][line] = 1;
             }
 
-            for (int line = 2; line < neededLine; line++) {
+            for (int line = 2; line < neededLines; line++) {
                 for (int cursor = 1; cursor < line; cursor++) {
                     triangle[line][cursor] = triangle[line - 1][cursor - 1] + triangle[line - 1][cursor];
                 }
             }
 
-            for (int line = 0; line < neededLine; line++) {
+            for (int cursor = 0; cursor <= neededLine; cursor++) {
+                Console.Write($" {triangle[neededLine][cursor]} ");
+            }
+
+            /*for (int line = 0; line < neededLines; line++) {
                 for (int cursor = 0; cursor <= line; cursor++) {
                     Console.Write($" {triangle[line][cursor]} ");
                 }
                 Console.WriteLine();
-            }
+            }*/
         }
     }
 }
