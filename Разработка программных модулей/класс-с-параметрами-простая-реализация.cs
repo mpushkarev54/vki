ff18da8b3679875a2d8d16750f051b7ba26161d0
mpushkarev54 <96192811+mpushkarev54@users.noreply.github.com>
2024-02-12 15:59:39 +0700

    Create класс-с-параметрами-простая-реализация.cs

---
+++ Разработка программных модулей/класс-с-параметрами-простая-реализация.cs
@@ -0,0 +1,47 @@
+﻿namespace ClassCar {
+    class Program {
+        static void Main() {
+            // без данных
+            Car car1 = new Car();
+            car1.PrintData();
+            car1.SaveToFile("car1.txt");
+
+            // с данными
+            Car car2 = new Car(154, "Toyota", "FunСargo", "н054ск154", "зеленый");
+            car2.PrintData();
+            car2.SaveToFile("car2.txt");
+        }
+    }
+
+    class Car {
+
+        public int id;
+        public string manufacturer;
+        public string model;
+        public string number;
+        public string color;
+
+        public Car() {
+            this.id = -1;
+            this.manufacturer = "N/A";
+            this.model = "N/A";
+            this.number = "N/A";
+            this.color = "N/A";
+        }
+        public Car(int id, string manufacturer, string model, string number, string color) {
+            this.id = id;
+            this.manufacturer = manufacturer;
+            this.model = model;
+            this.number = number;
+            this.color = color;
+        }
+
+        public void PrintData() {
+            Console.WriteLine($"{id}\t{manufacturer}\t{model}\t{number}\t{color}");
+        }
+
+        public void SaveToFile(string filename) {
+            File.WriteAllText(filename, $"{id}\t{manufacturer}\t{model}\t{number}\t{color}");
+        }
+    }
+}
